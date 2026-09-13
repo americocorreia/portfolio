f2c1ec438b3e64db008bf7219e02d38d3a187897
americocorreia <americogasparcorreia@gmail.com>
2026-09-13 12:42:52 +0100

    alterei uma funcionalidade no app.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 import Conteudo from "./Conteudo"; // Alterado para 'Conteudo' com C maiúsculo
 import "./app.css";
 import projetos from "./projectos/projetos";
-function App(){
+function App(){ 
   return (
     <div className="conteudo-principal">
       <Header />
